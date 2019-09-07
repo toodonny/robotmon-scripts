857dba250ac6c14c3300c4233daae40c076fdbca
toodonny <idhepp88@gmail.com>
2019-09-07 16:10:31 +0700

    D

--- scripts/com.donny.bladecrafter2/Script_updata_IDX_01_ weapenking_ 5558.bat
+++ scripts/com.donny.bladecrafter2/Script_updata_IDX_01_ weapenking_ 5558.bat
@@ -1,5 +1,5 @@
-.\updateadb\adb connect emulator-5558
-.\updateadb\adb -s emulator-5558 push .\images  /storage/emulated/legacy/Robotmon/scripts/com.donny.bladecrafter2/images
-.\updateadb\adb -s emulator-5558 push .\index.js  /storage/emulated/legacy/Robotmon/scripts/com.donny.bladecrafter2/
-.\updateadb\adb -s emulator-5558 push .\index.html  /storage/emulated/legacy/Robotmon/scripts/com.donny.bladecrafter2/
-.\updateadb\adb -s emulator-5558 shell rm -r /storage/emulated/legacy/Robotmon/scripts/com.donny.bladecrafter2/index.zip
+.\updateadb\adb connect emulator-5554
+.\updateadb\adb -s emulator-5554 push .\images  /storage/emulated/legacy/Robotmon/scripts/com.donny.bladecrafter2/images
+.\updateadb\adb -s emulator-5554 push .\index.js  /storage/emulated/legacy/Robotmon/scripts/com.donny.bladecrafter2/
+.\updateadb\adb -s emulator-5554 push .\index.html  /storage/emulated/legacy/Robotmon/scripts/com.donny.bladecrafter2/
+.\updateadb\adb -s emulator-5554 shell rm -r /storage/emulated/legacy/Robotmon/scripts/com.donny.bladecrafter2/index.zip
